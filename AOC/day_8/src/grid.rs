012b6753c555bfa7b2cca6f1d66ca6b726ca9fcc
JDN89 <jan.de.niels@gmail.com>
2023-04-11 22:20:28 +0200

    aoc-2022 day-8: create grid

--- AOC/day_8/src/grid.rs
+++ AOC/day_8/src/grid.rs
@@ -1,43 +1,38 @@
-use std::error::Error;
-use nom::bytes::complete::tag;
-use nom::character::complete::{char, newline};
-use nom::IResult;
-use nom::multi::separated_list1;
-
-// put in grid
-// loop over rows and collumns
-// for y in trees .len
-// for x in trees[0].len
-// then rev
-
-
-// for tomorrow-> convert char to digit
-
-//TODO: check error handling in from zero to production
-#[derive(Debug,PartialEq)]
-enum MyError {
+#[derive(Debug, PartialEq)]
+pub enum MyError {
     InvalidDigit(char),
 }
 
 pub fn to_num(c: char) -> Result<u32, MyError> {
-    match c.to_digit(10) {
+    match c.to_digit(10)
+
+    {
         Some(value) => Ok(value),
         None => Err(MyError::InvalidDigit(c)),
     }
 }
 
-pub fn create_grid(input: &str) -> Option<Vec<u32>> {
+pub fn create_grid(input: &str) -> Result<Vec<Vec<u32>>, MyError> {
+    let mut grid = vec![];
+
     for line in input.lines() {
-//TODO: do something with seperated_list and new line and parser to num
-        //check function parse crates
+        let row = line
+            .chars()
+            .map(to_num)
+            .collect::<Result<Vec<u32>, MyError>>()?;
+        grid.push(row);
     }
+    println!("{:?}", grid);
+    Ok(grid)
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-   fn convert_char_to_num() {
+    fn convert_char_to_num() {
         let input = '4';
         let result = to_num(input);
         assert_eq!(result, Ok(4))
---
+++ AOC/day_8/src/main.rs
@@ -0,0 +1,12 @@
+mod grid;
+
+    fn main() {
+
+        let input =  read_input().unwrap();
+        grid::create_grid(&input);
+    }
+
+    fn read_input() -> Result<String,std::io::Error> {
+        Ok(std::fs::read_to_string("src/test.txt")?)
+
+    }
