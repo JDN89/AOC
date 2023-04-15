f2cad21aff0e3e133251af58ab8fa1126a4d33b4
JDN89 <jan.de.niels@gmail.com>
2023-04-15 13:59:20 +0200

    iterate_y_axis_top_to_bottom plus write unit test

--- AOC/day_8/src/grid.rs
+++ AOC/day_8/src/grid.rs
@@ -84,6 +84,28 @@ pub fn iterate_x_axis_right_to_left(grid: &Vec<Vec<u32>>, visible_trees: &mut Ve
     visible_trees.to_vec()
 }
 
+pub fn iterate_y_axis_top_to_bottom(grid: &Vec<Vec<u32>>, visible_trees: &mut Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+    for row in 0..grid[0].len() {
+        let mut previous_tree_height = 0;
+        for col in 0..grid.len() {
+            println!("col: {}, row: {},previous tree height {},current tree height {}", col, row,previous_tree_height, grid[col][row]);
+            // we're counting in reverse order
+            if col == 0 {
+                println!("new col");
+                previous_tree_height = grid[col][row];
+                visible_trees[col][row] = true;
+            } else if grid[col][row] > previous_tree_height {
+                visible_trees[col][row] = true;
+                // println!(
+                //     "col: {}, row: {}, previous tree height: {}",
+                //     col, row, previous_tree_height
+                // );
+                previous_tree_height = grid[col][row];
+            }
+        }
+    }
+    visible_trees.to_vec()
+}
 
 #[cfg(test)]
 mod tests {
@@ -108,6 +130,23 @@ mod tests {
 
         let result = iterate_x_axis_left_to_right(&input, &mut visible_trees);
         let expected = vec![vec![true, true, true], vec![true, true, false], vec![true, true, true]];
+        assert_eq!(
+            result,expected
+        )
+    }
+    #[test]
+    fn test_iterate_y_axis_top_to_bottom() {
+        let input = vec![
+            vec![1, 2, 3],
+            vec![4, 5, 6],
+            vec![7, 3, 9],
+        ];
+
+        let mut visible_trees = vec![vec![false; input[0].len()]; input.len()];
+
+        let result = iterate_y_axis_top_to_bottom(&input, &mut visible_trees);
+        // check dat er op de juist plekken true staat
+        let expected = vec![vec![true, true, true], vec![true, true, true], vec![true, false, true]];
         assert_eq!(
             result,expected
         )
