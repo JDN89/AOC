b7198718d277138627d9297b92c841d98d56e18d
JDN89 <jan.de.niels@gmail.com>
2023-04-15 12:54:56 +0200

    iterate over x axis

--- AOC/day_8/src/grid.rs
+++ AOC/day_8/src/grid.rs
@@ -1,3 +1,7 @@
+use std::ops::Index;
+
+use nom::complete::bool;
+
 #[derive(Debug, PartialEq)]
 pub enum MyError {
     InvalidDigit(char),
@@ -25,19 +29,36 @@ pub fn create_grid(input: &str) -> Result<Vec<Vec<u32>>, MyError> {
     }
 
     grid_iterator(&grid);
-
-
-    println!("{:?}", grid);
     Ok(grid)
 }
 
-pub fn grid_iterator(grid: &Vec<Vec<u32>>)   {
-    for (x) in 0..grid[0].len() {
-        for (y) in 0..grid.len() {
-            println!("x: {}, y: {}, value: {}", x, y, grid[x][y]);
+pub fn grid_iterator(grid: &Vec<Vec<u32>>) {
+    //create a Vec<Vec<boolean>> where alle the values are set to true
+    //later we loop over the vec from left to right and right to left
+    //and top to bottom and bottom to top and we set the value to false
+    // if we encounter a tree that;s higher then the current tree
+
+    let mut visible_trees = vec![vec![false; grid[0].len()]; grid.len()];
+
+    // iteration for x axis
+    for col in 0..grid.len() {
+        let mut previous_tree_height = 0;
+        for row in 0..grid[0].len() {
+           // println!("position: row:{}, col:{} current tree height: {}", row, col, current_tree_height);
+            if row == 0 {
+                println!("new row");
+                previous_tree_height = grid[col][row];
+                visible_trees[col][row] = true;
+            }
+            else if grid[col][row] > previous_tree_height {
+                visible_trees[col][row] = true;
+                println!("col: {}, row: {}, previous tree height: {}", col, row, previous_tree_height);
+                previous_tree_height = grid[col][row];
+            }
         }
     }
 
+    println!("visible trees: {:?}", visible_trees);
 }
 
 
