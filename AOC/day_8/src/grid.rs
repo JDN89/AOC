5fa6fcd9df9da7de9d07938f05380d25c3a3f90f
JDN89 <jan.de.niels@gmail.com>
2023-04-16 09:57:23 +0200

    iterate_y_axis_bottom_to_top

--- AOC/day_8/src/grid.rs
+++ AOC/day_8/src/grid.rs
@@ -73,10 +73,10 @@ pub fn iterate_x_axis_right_to_left(grid: &Vec<Vec<u32>>, visible_trees: &mut Ve
                 visible_trees[col][row] = true;
             } else if grid[col][row] > previous_tree_height {
                 visible_trees[col][row] = true;
-                println!(
-                    "col: {}, row: {}, previous tree height: {}",
-                    col, row, previous_tree_height
-                );
+                // println!(
+                //     "col: {}, row: {}, previous tree height: {}",
+                //     col, row, previous_tree_height
+                // );
                 previous_tree_height = grid[col][row];
             }
         }
@@ -88,10 +88,33 @@ pub fn iterate_y_axis_top_to_bottom(grid: &Vec<Vec<u32>>, visible_trees: &mut Ve
     for row in 0..grid[0].len() {
         let mut previous_tree_height = 0;
         for col in 0..grid.len() {
-            println!("col: {}, row: {},previous tree height {},current tree height {}", col, row,previous_tree_height, grid[col][row]);
+            println!("col: {}, row: {},previous tree height {},current tree height {}", col, row, previous_tree_height, grid[col][row]);
             // we're counting in reverse order
             if col == 0 {
                 println!("new col");
+                previous_tree_height = grid[col][row];
+                visible_trees[col][row] = true;
+            } else if grid[col][row] > previous_tree_height {
+                visible_trees[col][row] = true;
+                // println!(
+                //     "col: {}, row: {}, previous tree height: {}",
+                //     col, row, previous_tree_height
+                // );
+                previous_tree_height = grid[col][row];
+            }
+        }
+    }
+    visible_trees.to_vec()
+}
+
+pub fn iterate_y_axis_bottom_to_top(grid: &Vec<Vec<u32>>, visible_trees: &mut Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+    for row in (0..grid[0].len()).rev() {
+        let mut previous_tree_height = 0;
+        for col in (0..grid.len()).rev() {
+            println!("col: {}, row: {},previous tree height {},current tree height {}", col, row, previous_tree_height, grid[col][row]);
+            // we're counting in reverse order
+            if col == grid.len() - 1 {
+                println!("new col: {}", col);
                 previous_tree_height = grid[col][row];
                 visible_trees[col][row] = true;
             } else if grid[col][row] > previous_tree_height {
@@ -131,9 +154,10 @@ mod tests {
         let result = iterate_x_axis_left_to_right(&input, &mut visible_trees);
         let expected = vec![vec![true, true, true], vec![true, true, false], vec![true, true, true]];
         assert_eq!(
-            result,expected
+            result, expected
         )
     }
+
     #[test]
     fn test_iterate_y_axis_top_to_bottom() {
         let input = vec![
@@ -148,7 +172,26 @@ mod tests {
         // check dat er op de juist plekken true staat
         let expected = vec![vec![true, true, true], vec![true, true, true], vec![true, false, true]];
         assert_eq!(
-            result,expected
+            result, expected
+        )
+    }
+
+    #[test]
+    //attention, we iterate bottom to top left to right, but the vec visbile trees is still filled in from left to right, top to bottom
+    fn test_iterate_over_y_axis_bottom_to_top() {
+        let input = vec![
+            vec![1, 2, 3],
+            vec![4, 5, 6],
+            vec![7, 3, 9],
+        ];
+
+        let mut visible_trees = vec![vec![false; input[0].len()]; input.len()];
+
+        let result = iterate_y_axis_bottom_to_top(&input, &mut visible_trees);
+        //
+        let expected = vec![vec![false, false, false], vec![false, true, false], vec![true, true, true]];
+        assert_eq!(
+            result, expected
         )
     }
 }
