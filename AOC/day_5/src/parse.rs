f69ff1113c8e9e4f46c22826cd7d85955155f80c
JDN89 <jan.de.niels@gmail.com>
2023-04-19 22:27:10 +0200

    aoc: day_5 init again

--- AOC/day_5/src/parse.rs
+++ AOC/day_5/src/parse.rs
@@ -1,6 +1,72 @@
+use nom::{IResult, bytes::complete::tag, character::complete::anychar, sequence::{delimited}, Parser};
+use nom::branch::alt;
+use nom::combinator::map;
+use nom::multi::{separated_list0, separated_list1};
+
+pub fn parse_crate(input: &str) -> IResult<&str, char> {
+    delimited(tag("["), anychar, tag("]"))(input)
+}
+pub fn parse_hole(input: &str) -> IResult<&str, ()> {
+    let (reminder,_) = tag("   ")(input)?;
+    Ok((reminder,()))
+}
+
+pub fn parse_crate_or_hole(input: &str) -> IResult<&str, Option<char>> {
+    let parse_crate_some = map(parse_crate, Some);
+    let parse_hole_none = map(parse_hole,|_| None);
+    alt((parse_crate_some, parse_hole_none))(input)
+}
 
 pub fn receive_input(input: &str)  {
     input.lines().for_each(|line| {
         println!("{:?}", line);
     });
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_receive_input() {
+        let input = "FBFBBFFRLR";
+        receive_input(&input);
+    }
+
+    #[test]
+    fn test_parse_crate_success() {
+        let input = "[A]";
+        let result = parse_crate(input);
+        assert_eq!(result, Ok(("", 'A')));
+    }
+
+    #[test]
+    fn test_parse_hole() {
+        let input = "   ";
+        let result = parse_hole(input);
+        assert_eq!(result, Ok(("",())))
+    }
+    #[test]
+    fn test_parse_crate_or_hole_crate() {
+        let input = "[A]";
+        let result = parse_crate_or_hole(input);
+        assert_eq!(result, Ok(("", Some('A'))));
+    }
+
+    #[test]
+    fn test_parse_crate_or_hole_hole() {
+        let input = "   ";
+        let result = parse_crate_or_hole(input);
+        assert_eq!(result, Ok(("", None)));
+    }
+
+    #[test]
+    fn test_parse_crate_or_hole_invalid() {
+        let input = "[AB]";
+        let result = parse_crate_or_hole(input);
+        assert!(result.is_err());
+    }
+
+
 }
