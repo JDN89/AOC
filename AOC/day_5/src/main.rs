03f5ee3bf1688a012bc0ed7393760b41f2ebfbad
JDN89 <jan.de.niels@gmail.com>
2023-04-22 19:51:45 +0200

    part II

--- AOC/day_5/src/main.rs
+++ AOC/day_5/src/main.rs
@@ -1,14 +1,14 @@
 use nom::combinator::all_consuming;
 use nom::Finish;
 
-use crate::parse::{Instruction, parse_instruction, parse_row};
+use crate::parse::{parse_instruction, parse_row, Instruction};
 use crate::transpose::transpose_rev;
 
 mod parse;
 mod transpose;
 
 fn main() {
-    let mut lines = include_str!("test.txt").lines();
+    let mut lines = include_str!("input.txt").lines();
 
     let crate_lines: Vec<_> = lines
         .by_ref()
@@ -27,29 +27,39 @@ fn main() {
     assert!(lines.next().unwrap().is_empty());
 
     let instructions: Vec<Instruction> = lines
-        .map(|line|
-            all_consuming(parse_instruction)(line).finish().unwrap().1)
+        .map(|line| all_consuming(parse_instruction)(line).finish().unwrap().1)
         .collect();
     for ins in &instructions {
-            for _p in 0..ins.mve {
-               let temp =  crate_columns[ins.from].pop();
-                println!("temp: {:?} ",temp);
-                match temp {
-                    Some(t) => crate_columns[ins.to].push(t),
-                    None => println!("stack is empty")
-                }
+            let mut temp_crate: Vec<char> = Vec::new();
+        for _p in 0..ins.mve {
 
+            let temp = crate_columns[ins.from].pop();
+
+            println!("temp: {:?} ", temp);
+            match temp {
+                Some(c) => temp_crate.push(c),
+                //Some(t) => crate_columns[ins.to].push(t),
+                None => println!("stack is empty"),
             }
         }
-    println!("result: {:?}", crate_columns);
-    let mut result:Vec<&char> = vec![];
-    let _ = crate_columns.iter().for_each(
-        |vec| match vec.last() {
-            Some(last_value) => result.push(last_value),
-            //println!("last: {:?}",last_value) ,
-            None => println!("no value"),
-        }
-    );
-    println!("result = {:?}",result);
+        temp_crate.reverse();
 
+        for c in temp_crate {
+            crate_columns[ins.to].push(c);
+        }
+
+        
     }
+    println!("result: {:?}", crate_columns);
+    let mut result: Vec<&char> = vec![];
+    let _ = crate_columns.iter().for_each(|vec| match vec.last() {
+        Some(last_value) => result.push(last_value),
+        //println!("last: {:?}",last_value) ,
+        None => println!("no value"),
+    });
+    let mut answer = String::new();
+    for ch in result {
+        answer.push(*ch);
+    }
+    println!("result = {:?}", answer);
+}
