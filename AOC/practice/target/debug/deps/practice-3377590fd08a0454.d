0d7c97aca459a6dacc66d066a6444ecbe0ec7e0c
JDN89 <jan.de.niels@gmail.com>
2023-08-09 20:56:17 +0200

    rust practice

---
+++ AOC/practice/target/debug/deps/practice-3377590fd08a0454.d
@@ -0,0 +1,5 @@
+/home/jan/Coding/AOC/AOC/practice/target/debug/deps/practice-3377590fd08a0454.rmeta: src/main.rs
+
+/home/jan/Coding/AOC/AOC/practice/target/debug/deps/practice-3377590fd08a0454.d: src/main.rs
+
+src/main.rs:
---
+++ AOC/practice/target/debug/deps/practice-bcab5e2d4ef1ea84.d
@@ -0,0 +1,5 @@
+/home/jan/Coding/AOC/AOC/practice/target/debug/deps/practice-bcab5e2d4ef1ea84.rmeta: src/main.rs
+
+/home/jan/Coding/AOC/AOC/practice/target/debug/deps/practice-bcab5e2d4ef1ea84.d: src/main.rs
+
+src/main.rs:
