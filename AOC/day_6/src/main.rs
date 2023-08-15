ab062d6010422534806bb1e64498aa273e71c31d
JDN89 <jan.de.niels@gmail.com>
2023-08-15 16:34:48 +0200

    restart AOC

--- AOC/day_6/src/main.rs
+++
@@ -1,7 +0,0 @@
-use crate::find_marker::find_marker;
-
-mod find_marker;
-
-fn main() {
-    dbg!(find_marker(include_str!("input.txt")));
-}
--- AOC/practice/target/debug/deps/practice-bcab5e2d4ef1ea84.d
+++
@@ -1,5 +0,0 @@
-/home/jan/Coding/AOC/AOC/practice/target/debug/deps/practice-bcab5e2d4ef1ea84.rmeta: src/main.rs
-
-/home/jan/Coding/AOC/AOC/practice/target/debug/deps/practice-bcab5e2d4ef1ea84.d: src/main.rs
-
-src/main.rs:
