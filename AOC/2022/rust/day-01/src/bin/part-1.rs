237647a7fdd4f6988a35347e7013baed02847613
JDN89 <jan.de.niels@gmail.com>
2023-08-15 20:37:19 +0200

    day 1 pt 1 cleanup

--- AOC/2022/rust/day-01/src/bin/part-1.rs
+++ AOC/2022/rust/day-01/src/bin/part-1.rs
@@ -3,7 +3,7 @@ use day_01::process_part1;
 
 fn main() {
     let total = fs::read_to_string("test_input.txt").unwrap();
-    let result = process_part1(&total);
+    let _result = process_part1(&total);
 
 
     // println!("{:?}", total);
