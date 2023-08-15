159a9c96fc5c469529b2c268d720a6a9da7c2310
JDN89 <jan.de.niels@gmail.com>
2023-08-15 16:33:51 +0200

    practice

--- AOC/practice/src/main.rs
+++ AOC/practice/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+use std::io;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
 fn main() {
-    println!("Hello, world!");
+    match read_username_from_file() {
+        Ok() => {}
+        Err(_) => {}
+    };
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    let sum = match y {
+        Some(value) => x + value,
+        None => x,
+    };
+    println!("Sum is: {}", sum);
 }
