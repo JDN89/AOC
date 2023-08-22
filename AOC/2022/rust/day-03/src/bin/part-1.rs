db4f932467eec0660ed8744e51a08ac4fefa8f99
JDN89 <jan.de.niels@gmail.com>
2023-08-22 22:11:50 +0200

    day-03 ptI run build

--- AOC/2022/rust/day-03/src/bin/part-1.rs
+++ AOC/2022/rust/day-03/src/bin/part-1.rs
@@ -2,7 +2,9 @@ use day_03::process_part1;
 use std::fs;
 
 fn main() {
+    println!("inside main");
     let total = fs::read_to_string("input.txt").unwrap();
     let result = process_part1(&total);
     println!("{:?}", result);
 }
+
