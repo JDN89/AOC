623fe24f621d6c8dd31124a20af2c7eb22ba544f
JDN89 <jan.de.niels@gmail.com>
2023-08-27 20:44:42 +0200

    failed attempt of parsing crates, restart tomorrow

--- AOC/2022/rust/day-05/src/bin/part-1.rs
+++ AOC/2022/rust/day-05/src/bin/part-1.rs
@@ -1,4 +1,4 @@
-use day_04::process_part1;
+use day_05::process_part1;
 use std::fs;
 
 fn main() {
--- AOC/2022/rust/day-05/src/lib.rs
+++ AOC/2022/rust/day-05/src/lib.rs
@@ -1,3 +1,28 @@
+// parse extern crate
+// parse hole: 3 spaces
+// parse crate and hole
+// parse row num
+// parse moves
+//
+use nom::branch::alt;
+use nom::character::complete::alpha1;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{anychar, char},
+    sequence::delimited,
+    IResult,
+};
+
+fn parse_crate(input: &str) -> IResult<&str, Option<&str>> {
+    let (input, c) = alt((tag("    "), delimited(char('['), alpha1, char(']'))))(input)?;
+
+    let result = match c {
+        "    " => None,
+        value => Some(value),
+    };
+    Ok((input, result))
+}
+
 pub fn process_part1(input: &str) -> &str {
     todo!()
 }
@@ -21,4 +46,9 @@ move 1 from 1 to 2";
     fn test_day1_part1() {
         assert_eq!(process_part1(INPUT), "CMZ");
     }
+    #[test]
+    fn test_parse_crate() {
+        assert_eq!(parse_crate("    [A]"), Ok(("", Some("A"))));
+    }
 }
+
