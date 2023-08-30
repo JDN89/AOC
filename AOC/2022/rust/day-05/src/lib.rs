198602bd2b4b12c678762e244786212b7f726afd
JDN89 <jan.de.niels@gmail.com>
2023-08-30 22:01:01 +0200

    day-05: partialy implemented parser

--- AOC/2022/rust/day-05/src/lib.rs
+++ AOC/2022/rust/day-05/src/lib.rs
@@ -4,27 +4,36 @@
 // parse row num
 // parse moves
 //
+//parse crate -> [A] Option crate
+//parse hole -> "    " return none
+//parse crate or hole -> option &char
+//when all the vectors have the same lenght you can transpose them
+//
+//Transposing a matrix means flipping it over its diagonal, which in turn means swapping the row and column indices for each element. In other words, the element that was in the ith row and jth column of the original matrix will end up in the jth row and ith column of the transposed matrix.
+//
 use nom::branch::alt;
-use nom::character::complete::alpha1;
+use nom::character::complete::{alpha1, multispace1};
+use nom::multi::many0;
+use nom::sequence::preceded;
 use nom::{
     bytes::complete::tag,
     character::complete::{anychar, char},
+    combinator::map,
     sequence::delimited,
     IResult,
 };
 
-//parse crate -> [A] Option crate
-//parse hole -> "    " return none
-//parse crate or hole -> option &char
+todo!()
+// how do i return nothing instead of an empty string?
+fn parse_crate(input: &str) -> IResult<&str, &str> {
+    delimited(char('['), alpha1, char(']'))(input)
+}
 
-fn parse_crate(input: &str) -> IResult<&str, Option<&str>> {
-    let (input, c) = alt((tag("    "), delimited(char('['), alpha1, char(']'))))(input)?;
-
-    let result = match c {
-        "    " => None,
-        value => Some(value),
-    };
-    Ok((input, result))
+fn parse_hole(input: &str) -> IResult<&str, &str> {
+    map(multispace1, |_| "")(input)
+}
+fn parse_crate_or_hole(input: &str) -> IResult<&str, Vec<&str>> {
+    many0(alt((parse_crate, parse_hole)))(input)
 }
 
 pub fn process_part1(input: &str) -> &str {
@@ -45,14 +54,30 @@ move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2";
+    const CRATE_OR_HOLE: &str = "    [D]    ";
 
-    #[test]
+    /* #[test]
     fn test_day1_part1() {
         assert_eq!(process_part1(INPUT), "CMZ");
-    }
+    } */
+
     #[test]
     fn test_parse_crate() {
-        assert_eq!(parse_crate("    [A]"), Ok(("", Some("A"))));
+        assert_eq!(parse_crate("[A]"), Ok(("", ("A"))));
+    }
+    #[test]
+    fn test_parse_hole() {
+        assert_eq!(parse_hole("    [A]"), Ok(("[A]", "")));
+    }
+    #[test]
+    fn test_parse_crate_or_hole() {
+        assert_eq!(
+            parse_crate_or_hole("[N] [C]    "),
+            Ok(("", vec!["N", "", "C", ""]))
+        );
+        assert_eq!(
+            parse_crate_or_hole(CRATE_OR_HOLE),
+            Ok(("", vec!["", "D", ""]))
+        );
     }
 }
-
