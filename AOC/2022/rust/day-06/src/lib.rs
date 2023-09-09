029a4972a0fb4c4b5b279efbb8775bedebb6c2cc
JDN89 <jan.de.niels@gmail.com>
2023-09-09 09:50:04 +0200

    solution part1v2  day6

--- AOC/2022/rust/day-06/src/lib.rs
+++ AOC/2022/rust/day-06/src/lib.rs
@@ -13,15 +13,34 @@ pub fn process_part1(input: &str) -> Option<usize> {
     None
 }
 
+pub fn process_part1_other_solution(i: &str) -> Option<usize> {
+    i.as_bytes()
+        .windows(4)
+        .position(|window| window.iter().collect::<HashSet<_>>().len() == 4)
+        .map(|position| position + 4)
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use test_case::test_case;
 
-    // Define a constant input for ease of use
-    const INPUT: &str = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+    use crate::{process_part1, process_part1_other_solution};
+    #[test_case(7, "mjqjpqmgbljsphdztnvjfqwrcgsmlb")]
+    #[test_case(5, "bvwbjplbgvbhsrlpgdmjqwftvncz")]
+    #[test_case(6, "nppdvjthqldpwncqszvftbrmjlhg")]
+    #[test_case(10, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")]
+    #[test_case(11, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")]
 
-    #[test]
-    fn test_day6_part1() {
-        assert_eq!(process_part1(INPUT), Some(10));
+    fn test_find_marker(index: usize, input: &str) {
+        assert_eq!(Some(index), process_part1(input))
+    }
+
+    #[test_case(7, "mjqjpqmgbljsphdztnvjfqwrcgsmlb")]
+    #[test_case(5, "bvwbjplbgvbhsrlpgdmjqwftvncz")]
+    #[test_case(6, "nppdvjthqldpwncqszvftbrmjlhg")]
+    #[test_case(10, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")]
+    #[test_case(11, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")]
+    fn test_find_marker_v2(index: usize, input: &str) {
+        assert_eq!(Some(index), process_part1_other_solution(input))
     }
 }
