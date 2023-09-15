5aeba8676c8015414d01daba1c6cadc3066637a7
JDN89 <jan.de.niels@gmail.com>
2023-09-15 16:59:01 +0200

    collect input in to a vec of vec u32

--- AOC/2022/rust/day-08/src/lib.rs
+++ AOC/2022/rust/day-08/src/lib.rs
@@ -1,5 +1,13 @@
-pub fn process_part1(input: &str) -> Option<usize> {
-    let temp: Vec<_> = input.lines().collect();
+pub fn process_part1(input: &str) -> Option<u32> {
+    let temp: Vec<_> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).expect("not a character"))
+                .collect::<Vec<u32>>()
+        })
+        .collect();
+
     dbg!(temp);
     Some(3)
 }
@@ -17,7 +25,7 @@ mod tests {
 33549
 35390"
     )]
-    fn find_visible_trees(index: usize, input: &str) {
-        assert_eq!(Some(index), process_part1(input))
+    fn find_visible_trees(visible_trees: u32, input: &str) {
+        assert_eq!(Some(visible_trees), process_part1(input))
     }
 }
