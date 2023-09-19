6dc249b5e92d1c53dcce531421cb345251567051
JDN89 <jan.de.niels@gmail.com>
2023-09-19 22:41:09 +0200

    it's itter time

--- AOC/2022/rust/day-08/src/lib.rs
+++ AOC/2022/rust/day-08/src/lib.rs
@@ -1,5 +1,14 @@
+fn iterate_left_to_right(trees: &Vec<Vec<u32>>, visible_trees: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+    for row in trees {
+        for &tree in row {
+            dbg!(tree);
+        }
+    }
+    visible_trees.to_owned()
+}
+
 pub fn process_part1(input: &str) -> Option<u32> {
-    let temp: Vec<_> = input
+    let trees: Vec<Vec<u32>> = input
         .lines()
         .map(|line| {
             line.chars()
@@ -8,24 +17,53 @@ pub fn process_part1(input: &str) -> Option<u32> {
         })
         .collect();
 
-    dbg!(temp);
+    // instantiate a vec of vec of booleans fo the same size and set all the trees to true
+    // iteratie left to right and right to left
+    // itereate top to bottom and bottom to top
+    // set the trees that are ont visible to false
+    // loop over grid and count number of trees that is said to true
+
+    let len = trees[0].len();
+    let mut visible_trees = vec![vec![true; len]; len];
+
     Some(3)
 }
 
 #[cfg(test)]
 mod tests {
-    use test_case::test_case;
+    use super::*;
+    use lazy_static::lazy_static;
 
-    use crate::process_part1;
-    #[test_case(
-        21,
-        "30373
+    const INPUT: &str = "30373
 25512
-65332
+25532
 33549
-35390"
-    )]
-    fn find_visible_trees(visible_trees: u32, input: &str) {
-        assert_eq!(Some(visible_trees), process_part1(input))
+35390";
+
+    lazy_static! {
+        static ref GRID: Vec<Vec<u32>> = vec![vec![1, 2, 3], vec![4, 5, 3], vec![7, 8, 9]];
+        static ref INITIAL: Vec<Vec<bool>> = vec![vec![true; 5]; 5];
+    }
+
+    const VISIBLE_TREES_LEFT_TO_RIGHT: &[&[bool]] = &[
+        &[true, true, true],
+        &[true, true, false],
+        &[true, true, true],
+    ];
+
+    #[test]
+    fn test_day1_part1() {
+        assert_eq!(process_part1(INPUT), Some(21));
+    }
+
+    #[test]
+    fn test_iterate_left_to_right() {
+        let input = vec![vec![1, 2, 3], vec![4, 5, 3], vec![7, 8, 9]];
+
+        let visible_trees = vec![vec![true; input[0].len()]; input.len()];
+        assert_eq!(
+            iterate_left_to_right(&input, &visible_trees),
+            VISIBLE_TREES_LEFT_TO_RIGHT
+        );
     }
 }
