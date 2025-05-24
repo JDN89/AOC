e233b636b7c18b2b09396d219d898be72039d43b
JDN89 <jan.de.niels@gmail.com>
2025-05-24 22:23:01 +0200

    ready for day 1

--- AOC/2024/rust/src/main.rs
+++ AOC/2024/rust/src/main.rs
@@ -4,7 +4,7 @@ mod util;
 fn main() {
     let day = std::env::args().nth(1).expect("Povide day number");
     match day.as_str() {
-        "1" => days::day01::run(),
+        "inputs/day01.txt" => days::day01::run(),
         // "2" => days::day02::run(),
         _ => eprintln!("Unknown day: {}", day),
     }
