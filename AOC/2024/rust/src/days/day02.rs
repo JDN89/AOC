c357d2b51555da72f6a812c3e7ee949ac092b157
JDN89 <jan.de.niels@gmail.com>
2025-06-04 13:04:52 +0200

    cleanup day 2 part 1

--- AOC/2024/rust/src/days/day02.rs
+++ AOC/2024/rust/src/days/day02.rs
@@ -10,51 +10,88 @@ pub fn run() {
 
 pub fn part1(input: &str) -> i32 {
     let mut safe_reports: i32 = 0;
-    let temp: Vec<Vec<i32>> = input
-        .lines()
-        .map(|line| {
-            let nums: Vec<i32> = line
-                .split_whitespace()
-                .filter_map(|x| x.parse().ok())
-                .collect();
+    for line in input.lines() {
+        let nums: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|l| l.parse().ok())
+            .collect();
 
-            for i in 0..nums.len() - 1 {
-                println!("i {}, nums[i] {}, nums[i+1] {}", i, nums[i], nums[i + 1]);
-                if i == nums.len() - 2 {
-                    let diff = (nums[i] - nums[i + 1]).abs();
-                    if diff < 1 || diff > 3 {
-                        break;
-                    } else {
-                        safe_reports += 1;
-                        break;
-                    }
-                }
+        //continue to the next row if num.len < 2
+        if nums.len() < 2 {
+            continue;
+        }
 
-                if nums[i] <= nums[i + 1] {
-                    println!("increasing");
-                    if nums[i + 1] >= nums[i + 2] {
-                        break;
-                    }
-                    let diff = (nums[i] - nums[i + 1]).abs();
-                    if diff < 1 || diff > 3 {
-                        break;
-                    }
-                } else {
-                    println!("decreasing");
-                    if nums[i + 1] <= nums[i + 2] {
-                        break;
-                    }
-                    let diff = (nums[i] - nums[i + 1]).abs();
-                    if diff < 1 || diff > 3 {
-                        break;
-                    }
-                }
+        let is_increasing = nums[1] > nums[0];
+        let mut is_safe = true;
+
+        for pair in nums.windows(2) {
+            let diff = (pair[1] - pair[0]).abs();
+            if diff < 1 || diff > 3 {
+                is_safe = false;
+                break;
             }
-            nums
-        })
-        .collect();
+            if is_increasing && pair[1] <= pair[0] {
+                is_safe = false;
+                break;
+            } else if !is_increasing && pair[1] >= pair[0] {
+                is_safe = false;
+                break;
+            }
+        }
+        if is_safe {
+            safe_reports += 1;
+        }
+    }
     safe_reports
 }
+//OLD solution->looks messy
+// pub fn part1(input: &str) -> i32 {
+//     let mut safe_reports: i32 = 0;
+//     let temp: Vel<Vec<i32>> = input
+//         .lines()
+//         .map(|line| {
+//             let nums: Vec<i32> = line
+//                 .split_whitespace()
+//                 .filter_map(|x| x.parse().ok())
+//                 .collect();
+//
+//             for i in 0..nums.len() - 1 {
+//                 println!("i {}, nums[i] {}, nums[i+1] {}", i, nums[i], nums[i + 1]);
+//                 if i == nums.len() - 2 {
+//                     let diff = (nums[i] - nums[i + 1]).abs();
+//                     if diff < 1 || diff > 3 {
+//                         break;
+//                     } else {
+//                         safe_reports += 1;
+//                         break;
+//                     }
+//                 }
+//
+//                 if nums[i] <= nums[i + 1] {
+//                     println!("increasing");
+//                     if nums[i + 1] >= nums[i + 2] {
+//                         break;
+//                     }
+//                     let diff = (nums[i] - nums[i + 1]).abs();
+//                     if diff < 1 || diff > 3 {
+//                         break;
+//                     }
+//                 } else {
+//                     println!("decreasing");
+//                     if nums[i + 1] <= nums[i + 2] {
+//                         break;
+//                     }
+//                     let diff = (nums[i] - nums[i + 1]).abs();
+//                     if diff < 1 || diff > 3 {
+//                         break;
+//                     }
+//                 }
+//             }
+//             nums
+//         })
+//         .collect();
+//     safe_reports
+// }
 
 pub fn part2(input: &str) -> i32 {
     2
