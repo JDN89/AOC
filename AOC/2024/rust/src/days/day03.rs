cde327656bec4cd1683e7168fa6084f95c0bd482
Jan DeNiels <jan.deniels@axians.com>
2025-06-26 22:42:42 +0200

    rewrite day3 part1

--- AOC/2024/rust/src/days/day03.rs
+++ AOC/2024/rust/src/days/day03.rs
@@ -9,26 +9,16 @@ pub fn run() {
     // println!("\nsolution part 2 : {}", part2_solution);
 }
 
-// struct Ins {
-//     operand1: i32,
-//     operand2: i32,
-//     result: i32,
-// }
-
 pub fn part1(input: &str) -> i32 {
-    // let mut instructions: Vec<Ins> = Vec::new();
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
-    let re = Regex::new(r"\w*mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    let mut result = 0;
-    for caps in re.captures_iter(input) {
-        let op1: i32 = caps[1].parse().unwrap();
-        let op2: i32 = caps[2].parse().unwrap();
-        let mul = op1 * op2;
-        result = result + mul;
-    }
-
-    println!("{}", &result);
-    result
+    re.captures_iter(input)
+        .map(|caps| {
+            let op1: i32 = caps[1].parse().unwrap();
+            let op2: i32 = caps[2].parse().unwrap();
+            op1 * op2
+        })
+        .sum()
 }
 
 pub fn part2(input: &str) -> i32 {
