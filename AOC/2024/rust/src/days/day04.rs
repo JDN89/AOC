1443d098016fd315d713f9ff63baa186b966485a
JDN89 <jan.de.niels@gmail.com>
2025-07-09 21:41:42 +0200

    remove print statements

--- AOC/2024/rust/src/days/day04.rs
+++ AOC/2024/rust/src/days/day04.rs
@@ -14,7 +14,7 @@ fn check_horizontal_right(grid: &Vec<Vec<char>>, x: usize, y: usize) -> i32 {
         return 0;
     }
     if grid[x][y + 1] == 'M' && grid[x][y + 2] == 'A' && grid[x][y + 3] == 'S' {
-        println!(" Horizontal left to right -- x: {}, y: {}", x, y);
+        // println!(" Horizontal left to right -- x: {}, y: {}", x, y);
         return 1;
     }
     return 0;
@@ -26,7 +26,7 @@ fn check_horizontal_left(grid: &Vec<Vec<char>>, x: usize, y: usize) -> i32 {
         return 0;
     }
     if grid[x][y - 1] == 'M' && grid[x][y - 2] == 'A' && grid[x][y - 3] == 'S' {
-        println!(" Horizontal right to left -- x: {}, y: {}", x, y);
+        // println!(" Horizontal right to left -- x: {}, y: {}", x, y);
         return 1;
     }
     return 0;
@@ -50,7 +50,7 @@ fn check_vertical_bottom_to_top(grid: &Vec<Vec<char>>, x: usize, y: usize) -> i3
         return 0;
     }
     if grid[x - 1][y] == 'M' && grid[x - 2][y] == 'A' && grid[x - 3][y] == 'S' {
-        println!("Vertical bottom to top -- x: {}, y: {}", x, y);
+        // println!("Vertical bottom to top -- x: {}, y: {}", x, y);
         return 1;
     }
     return 0;
@@ -70,7 +70,7 @@ fn check_diagnoal_top_right_left(grid: &Vec<Vec<char>>, x: usize, y: usize) -> i
         return 0;
     }
     if grid[x + 1][y - 1] == 'M' && grid[x + 2][y - 2] == 'A' && grid[x + 3][y - 3] == 'S' {
-        println!("Diagonal top to bottom right to left -- x: {}, y: {}", x, y);
+        // println!("Diagonal top to bottom right to left -- x: {}, y: {}", x, y);
         return 1;
     }
     return 0;
@@ -81,7 +81,7 @@ fn check_diagnoal_bottom_right_left(grid: &Vec<Vec<char>>, x: usize, y: usize) -
         return 0;
     }
     if grid[x - 1][y - 1] == 'M' && grid[x - 2][y - 2] == 'A' && grid[x - 3][y - 3] == 'S' {
-        println!("Diagonal bottom to top right to left -- x: {}, y: {}", x, y);
+        // println!("Diagonal bottom to top right to left -- x: {}, y: {}", x, y);
         return 1;
     }
     return 0;
@@ -92,7 +92,7 @@ fn check_diagnoal_bottom_left_right(grid: &Vec<Vec<char>>, x: usize, y: usize) -
         return 0;
     }
     if grid[x - 1][y + 1] == 'M' && grid[x - 2][y + 2] == 'A' && grid[x - 3][y + 3] == 'S' {
-        println!("Diagonal bottom to top left to right -- x: {}, y: {}", x, y);
+        // println!("Diagonal bottom to top left to right -- x: {}, y: {}", x, y);
         return 1;
     }
     return 0;
