863ee82bc40b0191dbf62f540c04c56b8a1b5058
JDN89 <jan.de.niels@gmail.com>
2025-07-14 23:01:31 +0200

    init day 5 part I

--- AOC/2024/rust/src/days/day05.rs
+++ AOC/2024/rust/src/days/day05.rs
@@ -11,23 +11,28 @@ pub fn run() {
 }
 
 pub fn part1(input: &str) -> i32 {
-    let mut test: HashMap<u32, Vec<u32>> = HashMap::new();
+    let mut ordering_rules: HashMap<u32, Vec<u32>> = HashMap::new();
     for line in input.lines() {
         if let Some((left, right)) = line.split_once('|') {
             if let (Ok(k), Ok(v)) = (left.trim().parse::<u32>(), right.trim().parse::<u32>()) {
-                test.entry(k).or_default().push(v);
-                println!("inserted {} -> {}", k, v);
+                ordering_rules.entry(k).or_default().push(v);
+                // println!("inserted {} -> {}", k, v);
             }
         } else if line.contains(',') {
             let page_updates: Vec<u32> = line.split(',').filter_map(|n| n.parse().ok()).collect();
-
-            println!("page_updates , {:?}", page_updates);
+            for update in &page_updates {
+                match ordering_rules.get(update) {
+                    Some(values) => println!("found key {} with values {:?}", update, values),
+                    None => println!("no entry for key {}", update),
+                }
+            }
         }
     }
+
     // Debug print of the final HashMap
-    for (k, v) in &test {
-        println!("{} -> {:?}", k, v);
-    }
+    // for (k, v) in &ordering_rules {
+    //     println!("{} -> {:?}", k, v);
+    // }
     1
 }
 
