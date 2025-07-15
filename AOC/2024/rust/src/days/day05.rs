3980d6a6b765d44769ad1df9eaf4f137e63e5763
JDN89 <jan.de.niels@gmail.com>
2025-07-15 22:27:34 +0200

    solution day5 part I

--- AOC/2024/rust/src/days/day05.rs
+++ AOC/2024/rust/src/days/day05.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::HashMap, env::var};
 
 use crate::util;
 
@@ -10,21 +10,49 @@ pub fn run() {
     println!("\nsolution part 2 : {}", part2_solution);
 }
 
-pub fn part1(input: &str) -> i32 {
+pub fn part1(input: &str) -> u32 {
     let mut ordering_rules: HashMap<u32, Vec<u32>> = HashMap::new();
+    let mut counter = 0;
     for line in input.lines() {
+        let mut is_order_correct = true;
         if let Some((left, right)) = line.split_once('|') {
             if let (Ok(k), Ok(v)) = (left.trim().parse::<u32>(), right.trim().parse::<u32>()) {
                 ordering_rules.entry(k).or_default().push(v);
                 // println!("inserted {} -> {}", k, v);
             }
         } else if line.contains(',') {
-            let page_updates: Vec<u32> = line.split(',').filter_map(|n| n.parse().ok()).collect();
-            for update in &page_updates {
-                match ordering_rules.get(update) {
-                    Some(values) => println!("found key {} with values {:?}", update, values),
-                    None => println!("no entry for key {}", update),
+            let page_updates: Vec<u32> = line
+                .split(',')
+                .filter_map(|n| n.parse().ok())
+                .rev()
+                .collect();
+            for window in page_updates.windows(2) {
+                let curr_update = window[0];
+                let next_update = window[1];
+                match &ordering_rules.get(&curr_update) {
+                    Some(values) => {
+                        if values.contains(&next_update) {
+                            println!(
+                                "order is wrong for curr {} next {}",
+                                curr_update, next_update
+                            );
+                            is_order_correct = false;
+                            break;
+                        } else {
+                            println!(
+                                "ordering might be correct for curr {}, next {} ",
+                                curr_update, next_update
+                            )
+                        }
+                    }
+                    None => println!("ket {} doesn't have a value", curr_update),
                 }
+            }
+            if is_order_correct {
+                let mid = page_updates.len() / 2;
+                let value = *page_updates.get(mid).unwrap_or(&0);
+                counter += value;
+                println!("line is correct {:?}", line);
             }
         }
     }
@@ -33,7 +61,7 @@ pub fn part1(input: &str) -> i32 {
     // for (k, v) in &ordering_rules {
     //     println!("{} -> {:?}", k, v);
     // }
-    1
+    counter
 }
 
 pub fn part2(input: &str) -> i32 {
