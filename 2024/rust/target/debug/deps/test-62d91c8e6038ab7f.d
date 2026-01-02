7650980bbb997363c52f4bfb9861d4085de4d539
JDN89 <jan.de.niels@gmail.com>
2026-01-02 18:45:17 +0100

    remove build files

--- 2024/rust/target/debug/deps/test-62d91c8e6038ab7f.d
+++
@@ -1,5 +0,0 @@
-/home/jan/Repos/AOC/2024/rust/target/debug/deps/test-62d91c8e6038ab7f.d: tests/test.rs
-
-/home/jan/Repos/AOC/2024/rust/target/debug/deps/libtest-62d91c8e6038ab7f.rmeta: tests/test.rs
-
-tests/test.rs:
